d38472753e7aaed924cd2da6ef94566c93c5171d
Pin-Yu Wang <vito1218@gmail.com>
2022-06-04 12:15:56 +0800

    Next time: add some filter function in file.rs

--- src/file.rs
+++ src/file.rs
@@ -1,12 +1,22 @@
-struct MyFile {
+use std::path::PathBuf;
+
+#[derive(Debug)]
+pub struct MyFile {
   name: String,
-  dir_in: String,
+  parent_dir: String,
   size_bytes: u64,
+  depth: u32,
 }
 
 impl MyFile {
-  fn from_path(path: PathBuf) -> Reulst<Self> {
-    let metadata = fs::metadata(path)?;
-    metadata.created().unwrap();
+  pub fn from_path(path: PathBuf, depth: u32) -> MyFile {
+    MyFile {
+      name: String::from(path.file_name().unwrap().to_string_lossy()),
+      parent_dir: String::from(path.parent().unwrap().to_string_lossy()),
+      size_bytes: path.metadata().unwrap().len(),
+      depth: depth,
+    }
   }
+
+  // implement regex and other filter function
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+mod file;
+
 use std::env;
 use std::fs;
 use std::io;
+use std::path::{Path, PathBuf};
+use std::vec::Vec;
 
 use structopt::StructOpt;
 
@@ -12,10 +16,16 @@ struct Cli {
     pattern: String,
 
     #[structopt(short = "d", long, required = true)]
-    dirs: String,
+    dir: String,
 
     #[structopt(short = "o", long, required = true)]
     output: String,
+
+    #[structopt(long)]
+    depth: Option<u32>,
+
+    #[structopt(long)]
+    larger_than_mb: Option<u32>,
 }
 
 fn main() -> io::Result<()> {
@@ -24,15 +34,32 @@ fn main() -> io::Result<()> {
 
     println!(
         "pattern:{}, dirs: {}, output: {}",
-        args.pattern, args.dirs, args.output
+        args.pattern, args.dir, args.output
     );
 
-    // ? mark is a kind of error propagation mechanism in Rust
-    for entry in fs::read_dir(args.dirs)? {
-        let entry = entry?;
-        let path = entry.path();
-        println!("{}", path.to_string_lossy());
+    let mut result: Vec<file::MyFile> = Vec::new();
+
+    visit_dir(&Path::new(&args.dir), &mut result, 1)?;
+
+    for my_file in result {
+        println!("{:?}", my_file);
     }
 
+    Ok(())
+}
+
+fn visit_dir(dir: &Path, result: &mut Vec<file::MyFile>, depth: u32) -> io::Result<()> {
+    if dir.is_dir() {
+        // ? mark is a kind of error propagation mechanism in Rust
+        for entry in fs::read_dir(dir)? {
+            let entry = entry?;
+            let path = entry.path();
+            if path.is_dir() {
+                visit_dir(&path, result, depth + 1)?;
+            } else {
+                result.push(file::MyFile::from_path(path, depth));
+            }
+        }
+    }
     Ok(())
 }

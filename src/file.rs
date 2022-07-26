1c3af5e1588c7d3f2aacbc6398ee6bd8d5b9dd0f
Pin-Yu Wang <vito1218@gmail.com>
2022-07-26 20:38:18 +0800

    Done

--- src/file.rs
+++ src/file.rs
@@ -1,22 +1,105 @@
-use std::path::PathBuf;
+use regex::Regex;
+use std::fmt;
+use std::fs;
+use std::io;
+use std::io::prelude::*;
+use std::path::{Path, PathBuf};
+
+#[derive(Debug)]
+pub struct MyFiles {
+    files: Vec<MyFile>,
+}
 
 #[derive(Debug)]
 pub struct MyFile {
-  name: String,
-  parent_dir: String,
-  size_bytes: u64,
-  depth: u32,
+    path_buf: PathBuf,
+    size_bytes: u64,
+    depth: u32,
+}
+
+impl MyFiles {
+    pub fn from_dir(dir_str: &str) -> MyFiles {
+        let dir = Path::new(dir_str);
+        let init_depth = 1;
+        let mut files: Vec<MyFile> = Vec::new();
+
+        visit_dir(dir, init_depth, &mut files).unwrap();
+
+        MyFiles { files }
+    }
+
+    pub fn retain_match_reg_pattern(&mut self, pattern: &str) {
+        match Regex::new(pattern) {
+            Ok(re) => self
+                .files
+                .retain(|file| re.is_match(file.path_buf.to_str().unwrap())),
+            _ => println!("bad regular expression"),
+        }
+    }
+
+    pub fn retain_depth_less_than(&mut self, depth: u32) {
+        self.files.retain(|file| file.depth <= depth)
+    }
+
+    pub fn retain_size_larger_than(&mut self, size_in_mb: u64) {
+        let size_in_byte = size_in_mb * (2 << 19);
+        // println!("size_in_mb: {}", size_in_mb);
+        // println!("filter size: {}", size_in_byte);
+        self.files.retain(|file| file.size_bytes > size_in_byte)
+    }
+
+    pub fn output(&self, output_dir: Option<String>) -> std::io::Result<()> {
+        match output_dir {
+            Some(output) => {
+                let mut new_file = fs::File::create(output)?;
+                new_file.write_all(self.to_string().as_bytes())?;
+                Ok(())
+            }
+            None => {
+                println!("{}", self);
+                Ok(())
+            }
+        }
+    }
+}
+
+impl fmt::Display for MyFiles {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            self.files
+                .iter()
+                .map(|file| file.path_buf.to_str().unwrap().to_owned() + "\n")
+                .collect::<String>()
+        )
+    }
 }
 
 impl MyFile {
-  pub fn from_path(path: PathBuf, depth: u32) -> MyFile {
-    MyFile {
-      name: String::from(path.file_name().unwrap().to_string_lossy()),
-      parent_dir: String::from(path.parent().unwrap().to_string_lossy()),
-      size_bytes: path.metadata().unwrap().len(),
-      depth: depth,
+    pub fn from_path(path: PathBuf, depth: u32) -> MyFile {
+        MyFile {
+            size_bytes: path.metadata().unwrap().len(),
+            depth,
+            path_buf: path,
+        }
     }
-  }
 
-  // implement regex and other filter function
+    // implement regex and other filter function
+}
+
+fn visit_dir(dir: &Path, depth: u32, files: &mut Vec<MyFile>) -> io::Result<()> {
+    if dir.is_dir() {
+        // ? mark is an error propagation mechanism in Rust
+        for entry in fs::read_dir(dir)? {
+            let entry = entry?;
+            let path = entry.path();
+            if path.is_dir() {
+                visit_dir(&path, depth + 1, files)?;
+            } else {
+                files.push(MyFile::from_path(path, depth));
+            }
+        }
+    }
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
 mod file;
 
 use std::env;
-use std::fs;
 use std::io;
-use std::path::{Path, PathBuf};
-use std::vec::Vec;
 
 use structopt::StructOpt;
 
@@ -18,48 +15,40 @@ struct Cli {
     #[structopt(short = "d", long, required = true)]
     dir: String,
 
-    #[structopt(short = "o", long, required = true)]
-    output: String,
+    #[structopt(short = "o", long)]
+    output: Option<String>,
 
     #[structopt(long)]
     depth: Option<u32>,
 
     #[structopt(long)]
-    larger_than_mb: Option<u32>,
+    larger_than_mb: Option<u64>,
 }
 
 fn main() -> io::Result<()> {
     let args = Cli::from_args();
     println!("current executable: {:?}", env::current_exe());
 
-    println!(
-        "pattern:{}, dirs: {}, output: {}",
-        args.pattern, args.dir, args.output
-    );
-
-    let mut result: Vec<file::MyFile> = Vec::new();
-
-    visit_dir(&Path::new(&args.dir), &mut result, 1)?;
-
-    for my_file in result {
-        println!("{:?}", my_file);
+    match &args.output {
+        Some(output) => println!(
+            "pattern:{}, dir: {}, output: {}",
+            args.pattern, args.dir, output
+        ),
+        None => println!("pattern:{}, dir: {}", args.pattern, args.dir),
     }
 
-    Ok(())
-}
+    let mut my_files = file::MyFiles::from_dir(&args.dir);
+    my_files.retain_match_reg_pattern(&args.pattern);
 
-fn visit_dir(dir: &Path, result: &mut Vec<file::MyFile>, depth: u32) -> io::Result<()> {
-    if dir.is_dir() {
-        // ? mark is a kind of error propagation mechanism in Rust
-        for entry in fs::read_dir(dir)? {
-            let entry = entry?;
-            let path = entry.path();
-            if path.is_dir() {
-                visit_dir(&path, result, depth + 1)?;
-            } else {
-                result.push(file::MyFile::from_path(path, depth));
-            }
-        }
+    match args.depth {
+        Some(depth) => my_files.retain_depth_less_than(depth),
+        None => (),
     }
-    Ok(())
+
+    match args.larger_than_mb {
+        Some(size_in_mb) => my_files.retain_size_larger_than(size_in_mb),
+        None => (),
+    }
+
+    my_files.output(args.output)
 }
